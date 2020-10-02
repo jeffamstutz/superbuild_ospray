20d4254af4b8ba8c9a25f9945ad81ca09e24f61e
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2020-10-02 16:12:12 -0500

    fix macOS build

--- get_tbb.cmake
+++ get_tbb.cmake
@@ -6,10 +6,16 @@ set(TBB_VERSION v2020.3)
 
 if (APPLE)
   set(TBB_URL https://github.com/oneapi-src/oneTBB/releases/download/${TBB_VERSION}/${SUBPROJECT_NAME}-mac.tgz)
+  set(TBB_LIB_PATH "tbb/lib")
+  set(TBB_LIB_DESTINATION "lib")
 elseif(WIN32)
   set(TBB_URL https://github.com/oneapi-src/oneTBB/releases/download/${TBB_VERSION}/${SUBPROJECT_NAME}-win.zip)
+  set(TBB_LIB_PATH "tbb/bin")
+  set(TBB_LIB_DESTINATION "bin")
 else()
   set(TBB_URL https://github.com/oneapi-src/oneTBB/releases/download/${TBB_VERSION}/${SUBPROJECT_NAME}-lin.tgz)
+  set(TBB_LIB_PATH "tbb/lib/intel64/gcc4.8")
+  set(TBB_LIB_DESTINATION "lib")
 endif()
 
 ExternalProject_Add(tbb
@@ -21,8 +27,8 @@ ExternalProject_Add(tbb
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND "${CMAKE_COMMAND}" -E copy_directory
-    <SOURCE_DIR>/tbb/lib/intel64/gcc4.8
-    ${INSTALL_DIR_ABSOLUTE}/lib
+    <SOURCE_DIR>/${TBB_LIB_PATH}
+    ${INSTALL_DIR_ABSOLUTE}/${TBB_LIB_DESTINATION}
   BUILD_ALWAYS OFF
 )
 
